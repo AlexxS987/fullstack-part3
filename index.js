090245b1bf797f310c19c6e80b439fedc085600d
Aleksandr Skakun <m0405@student.jamk.fi>
2024-06-20 18:18:44 +0300

    Added GET and POST to db functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -20,77 +22,50 @@ app.use(
   })
 )
 
-function getRandomInt() {
-  const minCeiled = Math.ceil(1);
-  const maxFloored = Math.floor(100000);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
-}
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/info', (request, response) => {
-  const time = new Date().toLocaleString('en-FI');
-  response.send(`<p>Phonebook has infor for ${persons.length} people</p><p>${time}</p>`)
+  const time = new Date().toLocaleString('en-FI')
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name && !body.number) {
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'Name or phone number are missing' 
     })
   }
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'Name must be unique' 
+
+  Person.exists({ name: body.name }).then(exists => {
+    if (exists) {
+      return response.status(400).json({ 
+        error: 'Name must be unique' 
+      })
+    }
+
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
-  }
-
-  const person = {
-    id: getRandomInt(),
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)
-  response.json(person)
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -100,7 +75,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
